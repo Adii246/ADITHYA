504ea87b744ced048e2665bcc0635deb8557e52b
Adii246 <145443565+Adii246@users.noreply.github.com>
2023-09-19 15:12:41 +0530

    Add files via upload

---
+++ Hellojava.java
@@ -0,0 +1,16 @@
+/**********************************************************************************************
+ *File     :HelloJava.java
+ *Description : My first Java Program that prints "Hello Java"
+ *Author : Adithya Raj
+ *Version : 1.0
+ *Date : 18/09/2023
+ *****************************************************************************************/
+
+package java1;
+
+public class Hellojava {
+	public static void main(String [] args) {
+		System.out.println("Hello Java");
+		System.out.print("Haiii");
+}
+}
