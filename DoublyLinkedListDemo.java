40b38d0e14bca530376c9c3012ffa434366edbe1
ADITHYA RAJ <145443565+Adii246@users.noreply.github.com>
2023-12-05 11:34:48 +0530

    Add files via upload

--- DoublyLinkedListDemo.java
+++ DoublyLinkedListDemo.java
@@ -1,42 +1,88 @@
 package java_lab;
-
 import java.util.Scanner;
 import java.util.LinkedList;
-
-
 class DoublyLinkedList<T>{
 	private LinkedList < T > list= new LinkedList<>();
-	public void add(T element) 
+	public void insertAtFront(T element) {
+		list.addFirst(element);
+	}
+	public void insertAtEnd(T element) 
 	{
 		list.addLast(element);
 	}
-	public void insert(int index,T element) {
+	public void insertAtAnyposition(int index,T element) {
 		list.add(index,element);
 	}
-	public void remove(int index) {
+	public void removeAtAnyPosition(int index) {
 		list.remove(index);
 	}
 	public void display() {
 		for(T elements:list) {
-			System.out.println(elements+" ");
+			System.out.print(elements+" ");
 		}
+		System.out.println(" ");
 	}
 }
 public class DoublyLinkedListDemo {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		DoublyLinkedList <Integer> doublyLinkedList=new DoublyLinkedList <Integer> ();
-		doublyLinkedList.add(5);
-		doublyLinkedList.add(4);
-		doublyLinkedList.add(9);
-		doublyLinkedList.display();
-		doublyLinkedList.insert(1, 10);
-		doublyLinkedList.display();
-		doublyLinkedList.remove(2);
-		doublyLinkedList.display();
+		DoublyLinkedList <Integer> doublyLinkedList=new DoublyLinkedList <> ();
 		
-
+		int choice=0;
+		while(choice!=6) {
+			System.out.println("Enter your choice\n"+"1.Insertion At Front\n"
+					+ "2.Insertion At End\n"+"3.Insertion At any position\n"
+					+"4.Deletion at any Position\n"+"5. Display\n"
+					+"6.Exit\n");
+			 choice = sc.nextInt();
+			int element,position;
+			
+			switch(choice) {
+			case 1:
+				System.out.println("Enter the element:");
+				 element = sc.nextInt();
+				doublyLinkedList.insertAtFront(element);
+				break;
+			
+			case 2:
+				System.out.println("Enter the element:");
+				 element = sc.nextInt();
+				doublyLinkedList.insertAtEnd(element);
+				break;	
+			
+			case 3:
+				System.out.println("Enter the element:");
+				 element = sc.nextInt();
+				System.out.println("Enter the position:");
+				 position = sc.nextInt();
+				doublyLinkedList.insertAtAnyposition(position, element);
+				break;
+			case 4:
+				
+				System.out.println("Enter the position:");
+				 position = sc.nextInt();
+				doublyLinkedList.removeAtAnyPosition(position);
+				break;
+				
+			case 5:
+				System.out.println("The elements are :");
+				doublyLinkedList.display();
+				break;
+				
+			case 6:
+				System.out.println("Exit");
+			
+			default :
+				System.out.println("Invalid choice");
+				
+				 
+			
+			
+			
+		}
+		
 	}
 
+}
 }
